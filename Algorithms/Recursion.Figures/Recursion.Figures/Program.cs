08e730520594eef60c0509e9fb0c6d2c901b4f7b
Hristian86 <59567820+Hristian86@users.noreply.github.com>
2020-05-10 18:15:37 +0300

    algorithms

---
+++ Algorithms/Recursion.Figures/Recursion.Figures/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Recursion.Figures
+{
+    class Program
+    {
+        //        3. Recursive Drawing
+        //Write a program that draws the figure below depending on n.Use recursion.
+        static void Main(string[] args)
+        {
+
+            int number = int.Parse(Console.ReadLine());
+            PrintFigure(number);
+
+        }
+
+        private static void PrintFigure(int number)
+        {
+            if (number == 0)
+            {
+                return ;
+            }
+
+            Console.WriteLine(new string('*',number));
+
+            PrintFigure(number-1);
+
+            Console.WriteLine(new string('#',number));
+        }
+    }
+}
