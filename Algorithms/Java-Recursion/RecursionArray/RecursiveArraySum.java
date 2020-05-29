b82d0d480fba2e093c67531b171376862736086e
Hristian86 <59567820+Hristian86@users.noreply.github.com>
2020-05-29 15:54:55 +0300

    Java-Recursion

---
+++ Algorithms/Java-Recursion/RecursionArray/RecursiveArraySum.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * RecursiveArraySum
+ */
+public class RecursiveArraySum {
+
+//     1. Recursive Array Sum
+// Write a program that finds the sum of all elements in an integer array. Use recursion.
+// Note: In practice this recursion should not be used here (instead use an iterative solution), this is just an exercise.
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers  = Arrays.stream(scanner.nextLine().split(" "))
+                        .mapToInt(Integer::parseInt)
+                        .toArray();
+
+        // int[] numbers = new int[]{1,2,3,4};
+        int num = RecursiveArr(numbers,0);
+        System.out.print(num);
+    }
+
+    public static int RecursiveArr(int[] numbers,int index) {
+        if (index >= numbers.length) {
+            return 0;
+        }
+        return numbers[index] + RecursiveArr(numbers, index + 1);
+    }
+}
---
+++ Algorithms/Java-Recursion/RecursiveDrawing/RecDrawing.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+/**
+ * RecDrawing
+ */
+public class RecDrawing {
+
+//     2. Recursive Drawing
+// Write a program that draws the figure below depending on n. Use recursion.
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = Integer.parseInt(scanner.nextLine());
+        // int number = 5;
+        
+        Draw(number);
+    }
+
+    public static void Draw(int number){
+        if (number == 0) {
+            return;
+        }
+        for(int i = 0; i < number; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        
+        number--;
+        Draw(number);
+        number++;
+        for(int i = 0; i < number; i++) {
+            System.out.print("#");
+        }
+        System.out.println();
+        // System.out.println("*".repeat(number));
+    }
+}
