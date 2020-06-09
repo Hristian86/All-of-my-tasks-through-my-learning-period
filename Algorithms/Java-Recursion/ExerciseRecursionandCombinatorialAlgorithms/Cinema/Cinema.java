1ce180dd7f360f0c6beae7454e431fe4ecd05433
Hristian86 <59567820+Hristian86@users.noreply.github.com>
2020-06-09 16:58:11 +0300

    More recursion

--- Algorithms/Java-Recursion/ExerciseRecursionandCombinatorialAlgorithms/Cinema/Cinema.java
+++ Algorithms/Java-Recursion/ExerciseRecursionandCombinatorialAlgorithms/Cinema/Cinema.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.*;
+
 /**
  * Cinema
  */
@@ -14,8 +20,64 @@ public class Cinema {
 // they will sit there only. The order of the output does not matter.
 // Constrains
 //  The friends names and the number of the place will always be valid
-
+    private static List<String> people;
+    private static String[] combinations;
+    private static String[] seats;
+    private static boolean[] used;
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        people = Arrays.stream(scanner.nextLine().split(", "))
+        .collect(Collectors.toList());
+
+        seats = new String[people.size()];
+        String line = scanner.nextLine();
+
+        while (!line.equals("generate")) {
+            
+
+            String[] tokens = line.split(" - ");
+            String name = tokens[0];
+            int position = Integer.parseInt(tokens[1]) - 1;
+
+            seats[position] = name;
+            people.remove(name);
+
+            line = scanner.nextLine();
+        }
+
+        combinations = new String[people.size()];
+        used = new boolean[people.size()];
+
+        Permute(0);
+    }
+
+    private static void Permute(int index) {
+        if (index == combinations.length) {
+            Print();
+        } else {
+                for (int i = 0; i < people.size(); i++) {
+                    if (!used[i]) {
+                        used[i] = true;
+                            combinations[index] = people.get(i);
+                            Permute(index + 1);
+                            used[i] = false;
+                        }
+            }
+        }
+    }
+
+    private static void Print() {
+        int index = 0;
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < seats.length; i++) {
+            if (seats[i] != null) {
+                str.append(seats[i] + " ");
+            } else {
+                str.append(combinations[index++] + " ");
+            }
+        }
+        System.out.print(str.toString().trim());
+        System.out.println();
     }
 }
